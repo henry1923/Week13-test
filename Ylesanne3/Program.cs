c00f974302455ec70bbe97b87191c826a4686ef6
Opilane TTHK <opilane@tthk.ee>
2021-11-23 15:58:16 +0200

    1

---
+++ Ylesanne3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Ylesanne3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string StarsPath = @"\Users\opilane\samples\test\Stars.txt";
+            string PlanetsPath = @"\Users\opilane\samples\test\Planets.txt";
+            string MilkyWayPath = @"\Users\opilane\samples\test\MilkyWay.txt";
+            List<string> stars = new List<string>();
+            List<string> planets = new List<string>();
+
+            string[] sourceData = File.ReadAllLines(MilkyWayPath);
+
+            foreach (string element in sourceData)
+            {
+                if (element.Contains("star"))
+                {
+                    stars.Add(element);
+                }
+                if (element.Contains("planet"))
+                {
+                    planets.Add(element);
+                }
+                File.WriteAllLines(StarsPath, stars);
+                File.WriteAllLines(PlanetsPath, planets);
+            }
+        }
+    }
+}
